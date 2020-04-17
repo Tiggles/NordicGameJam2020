6a3c7e9596b546ba72945214c71f9352c404d1e4
Jonas Malte Hinchely <jonas@hinchely.dk>
2020-04-17 18:39:54 +0200

    Add more conf, add escape to close

--- conf.lua
+++ conf.lua
@@ -1,4 +1,10 @@
 function love.conf(t)
     t.window.width = 1024
     t.window.height = 768
+    t.version = "11.3"
+    t.window.title = "Everyday Magic"
+    t.window.borderless = false
+    t.window.vsync = 1
+    t.window.highdpi = true
+    t.console = true -- For windows
 end
--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ function love.load()
 end
 
 function love.update(delta)
-
+    if love.keyboard.isDown("escape") then love.event.quit() end
 end
 
 function love.draw()
