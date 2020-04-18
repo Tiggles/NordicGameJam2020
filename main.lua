00bf9a4f4761010d05c56f377d75568d3141fa69
Brian Alberg <brian@alberg.org>
2020-04-19 01:32:59 +0200

    Made it possible to switch between store and garden context

--- main.lua
+++ main.lua
@@ -30,12 +30,15 @@ local game_state = {
         decline = responses.decline[love.math.random(#responses.decline)]
     },
     cauldrons = {},
-    money = 200
+    money = 200,
+    current_location = "store"
 }
 
 local accept_box = {x = 500, y = 100, width = 345, height = 55}
 local postpone_box = {x = 500, y = accept_box.y + accept_box.height + 15, width = 345, height = 55}
 local decline_box = {x = 500, y = postpone_box.y + postpone_box.height + 15, width = 345, height = 55}
+local goto_garden_button = {x = 700, y = 628, width = 160, height = 38}
+local goto_store_button = {x = 7, y = 628, width = 185, height = 38}
 
 local update = function() end
 local draw = function() end
@@ -70,27 +73,42 @@ function love.update(delta)
     local active_customer = game_state.customers[1] ~= nil
     update()
 
-    if active_customer and next_action_allowed < love.timer.getTime() and love.mouse.isDown("1") then
-        local x, y = love.mouse.getPosition()
-        if is_colliding({x = x, y = y}, accept_box) then
-            local power = game_state.customers[1]:get_power()
-            local remaining_potions = get_remaining_potions(power)
-            if remaining_potions ~= 0 then
-                game_state.inventory:use_potion(power)
-                table.remove(game_state.customers, 1)
+    if game_state.current_location == "store" then
+        if love.mouse.isDown("1") then
+            local x, y = love.mouse.getPosition()
+            if active_customer and next_action_allowed < love.timer.getTime() then
+                if is_colliding({x = x, y = y}, accept_box) then
+                    local power = game_state.customers[1]:get_power()
+                    local remaining_potions = get_remaining_potions(power)
+                    if remaining_potions ~= 0 then
+                        game_state.inventory:use_potion(power)
+                        table.remove(game_state.customers, 1)
+                    end
+                    next_action_allowed = love.timer.getTime() + 0.2
+                elseif is_colliding({x=x, y=y}, postpone_box) then
+                    print("Postpone")
+                    table.insert(game_state.postponed_customers, game_state.customers[1])
+                    table.remove(game_state.customers, 1)
+                    next_action_allowed = love.timer.getTime() + 0.2
+                elseif is_colliding({x=x, y=y}, decline_box) then
+                    print("Decline")
+                    table.remove(game_state.customers, 1)
+                    next_action_allowed = love.timer.getTime() + 0.2
+                else
+                    print("None")
+                end
             end
-            next_action_allowed = love.timer.getTime() + 0.2
-        elseif is_colliding({x=x, y=y}, postpone_box) then
-            print("Postpone")
-            table.insert(game_state.postponed_customers, game_state.customers[1])
-            table.remove(game_state.customers, 1)
-            next_action_allowed = love.timer.getTime() + 0.2
-        elseif is_colliding({x=x, y=y}, decline_box) then
-            print("Decline")
-            table.remove(game_state.customers, 1)
-            next_action_allowed = love.timer.getTime() + 0.2
-        else
-            print("None")
+            if is_colliding({x=x, y=y}, goto_garden_button) then
+                game_state.current_location = "garden"
+            else
+            end
+        end
+    elseif game_state.current_location == "garden" then
+        if love.mouse.isDown("1") then
+            local x, y = love.mouse.getPosition()
+            if is_colliding({x=x, y=y}, goto_store_button) then
+                game_state.current_location = "store"
+            end
         end
     end
     -- At start of day, calculate when customers appear
@@ -102,16 +120,20 @@ end
 
 function love.draw()
     local active_customer = game_state.customers[1] ~= nil
-    love.graphics.draw(store, 0, 0, 0, 3, 3)
 
-    for i = 1, #game_state.customers do
-        local c = game_state.customers[i]
-        love.graphics.draw(customer_sprites[c.color], 190, 220 + i * 60, 0, 4, 4)
+    if game_state.current_location == "store" then
+        love.graphics.draw(store, 0, 0, 0, 3, 3)
+
+        for i = 1, #game_state.customers do
+            local c = game_state.customers[i]
+            love.graphics.draw(customer_sprites[c.color], 190, 220 + i * 60, 0, 4, 4)
+        end
+
+        draw_conversation(active_customer)
+        draw_inventory()
+    elseif game_state.current_location == "garden" then
+        love.graphics.draw(garden, 0, 0, 0, 3, 3)
     end
-
-    draw_conversation(active_customer)
-
-    draw_inventory()
 
     if game_state.paused then love.graphics.print("PAUSED. Press escape to unpause.") end
 end
