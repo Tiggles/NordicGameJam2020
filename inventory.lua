7d37a1c0e03e33aab44b984eb50ae74294c94d61
Brian Alberg <brian@alberg.org>
2020-04-19 13:19:47 +0200

    Added ingredients to inventory

--- inventory.lua
+++ inventory.lua
@@ -10,7 +10,11 @@ function Inventory:new()
             endurance_potions = 1
         },
         ingredients = {
-
+            spinach = 1,
+            coffee = 1,
+            cat_eyes = 1,
+            camel_hump = 1,
+            seaweed = 1
         },
         page = 1
     }
@@ -37,6 +41,35 @@ end
 function Inventory:get_nightvision()
     return self.potions.nightvision_potions
 end
+
+function Inventory:get_spinach()
+    return self.ingredients.spinach
+end
+
+function Inventory:get_coffee()
+    return self.ingredients.coffee
+end
+
+function Inventory:get_cat_eyes()
+    return self.ingredients.cat_eyes
+end
+
+function Inventory:get_camel_hump()
+    return self.ingredients.camel_hump
+end
+
+function Inventory:get_seaweed()
+    return self.ingredients.seaweed
+end
+
+
+
+
+
+
+
+
+
 
 function Inventory:has_next_page()
     return self.page == 1
@@ -83,5 +116,21 @@ function Inventory:add_potion(power)
         self.potions.endurance_potions = self.potions.endurance_potions + 1
     else
         print("#ERROR: " .. power " not handled")
+    end
+end
+
+function Inventory:add_ingredient(ingredient)
+    if ingredient == "spinach" then
+        self.ingredients.spinach = self.ingredients.spinach + 1
+    elseif ingredient == "coffee" then
+        self.ingredients.coffee = self.ingredients.coffee + 1
+    elseif ingredient == "cat_eyes" then
+        self.ingredients.cat_eyes = self.ingredients.cat_eyes + 1
+    elseif ingredient == "camel_hump" then
+        self.ingredients.camel_hump = self.ingredients.camel_hump + 1
+    elseif ingredient == "seaweed" then
+        self.ingredients.seaweed = self.ingredients.seaweed + 1
+    else
+        print("#ERROR: " .. ingredient " not handled")
     end
 end
